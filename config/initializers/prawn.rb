d39bdfae43fdaa27e34bc70e4f0e4ca40f40049b
juanlb <juan.labattaglia@snappler.com>
2018-02-02 18:29:21 -0300

    margenes segun acordada

--- config/initializers/prawn.rb
+++ config/initializers/prawn.rb
@@ -4,10 +4,10 @@ module Prawn
       def move_past_bottom
         if @document.page_number == @document.page_count
           if @document.page_number.odd?
-            @document.start_new_page(margin: [5.cm,5.cm,1.5.cm,2.cm])
+            @document.start_new_page(margin: [5.cm,5.cm,2.cm,1.5.cm])
                                               #top,right,bottom,left 
           else
-            @document.start_new_page(margin: [5.cm,2.cm,1.5.cm,5.cm])
+            @document.start_new_page(margin: [5.cm,1.5.cm,2.cm,5.cm])
           end
         else
           @document.go_to_page(@document.page_number + 1)
